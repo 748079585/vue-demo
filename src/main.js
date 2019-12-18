1e9f4dc9af9fb58b93fb0946f5fb6ef9f7a79185
luke <748079585@qq.com>
2019-12-18 10:44:43 +0800

    add router ,store and layout

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import store from './store'
+import router from './router'
+
 Vue.use(Element)
 
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
